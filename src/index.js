f5bec310bf0819e91ca3194887a7f9284e000225
ra <x.ksnxx@gmail.com>
2025-12-31 16:56:53 +0900

    Refactor React import and root rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
